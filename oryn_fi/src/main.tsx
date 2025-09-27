e49dcd8751d7299d2f2cab18b395352ae3d5141a
panugothrakesh <rakeshpanugoth2003@gmail.com>
2025-09-27 14:29:03 +0530

    ref

--- oryn_fi/src/main.tsx
+++ oryn_fi/src/main.tsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { AppKitProvider } from './providers/AppKitProviders.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppKitProvider>
+    {/* <AppKitProvider> */}
     <App />
-    </AppKitProvider>
-  </StrictMode>,
-)
+    {/* </AppKitProvider> */}
+  </StrictMode>
+);
