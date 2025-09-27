86fc9c6d9da5b1ad327a6752c759635f0213b31b
shanmukh0504 <shanmukh0504@gmail.com>
2025-09-27 13:59:16 +0530

    Remove AppKitProvider from main rendering in the application

--- oryn_fi/src/main.tsx
+++ oryn_fi/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { AppKitProvider } from './providers/AppKitProviders.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppKitProvider>
     <App />
-    </AppKitProvider>
   </StrictMode>,
 )
